f9679253531fc707b29f473ffc808454e6502744
Jacob Ceder <jacob.ceder@husqvarnagroup.com>
2024-01-10 15:08:43 +0100

    commit 1

--- site/script.js
+++ site/script.js
@@ -1,1 +1,8 @@
-console.log("Choo choo!");
+document.addEventListener('DOMContentLoaded', function() {
+    var arrow = document.getElementById('arrow');
+    var angle = 0;
+    setInterval(function() {
+        angle += 45; // Ändra vinkeln med 45 grader
+        arrow.style.transform = 'rotate(' + angle + 'deg)';
+    }, 200); // Uppdatera varje sekund
+});
